729f237a0464d1a8328f277135d6da3d6c61a9cd
Oren Novotny <oren@novotny.org>
2016-11-07 12:17:15 -0800

    Add test project

--- src/HelloRepo/Class1.cs
+++ src/HelloRepo/Class1.cs
@@ -7,5 +7,9 @@ namespace HelloRepo
 {
     public class Class1
     {
+        public void DoSomething()
+        {
+            
+        }
     }
 }
---
+++ test/HelloRepo.Tests/HelloRepoTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloRepo;
+using Xunit;
+
+
+public class HelloRepoTests
+{
+    [Fact]
+    public void HelloRepoDoesSomething()
+    {
+        var c1 = new Class1();
+        c1.DoSomething();
+
+    }
+}
